879492f65e3f42008cbc3d0630bd538270789e30
luizanjos88 <luizanjos88@hotmail.com>
2026-04-04 18:50:28 -0400

    First Scnario

--- cypress/e2e/main.spec.cy.js
+++ cypress/e2e/main.spec.cy.js
@@ -1,14 +1,26 @@
+const mensagem = 'Comentário de teste';
+
+
 describe('template spec', () => {
-  it('passes', () => {
+
+  beforeEach(() => {
     cy.visit('localhost:5173/')
-    cy.get('[data-qa="profile-box"]')
-    cy.get('[data-qa="comment-input"]').first().type('Test 123')
-    cy.get('[data-qa="publish-button"]').first().click()
-    cy.get('[data-qa="comment-text"]').contains('Test 123')
-    cy.get('[data-qa="comment-text"]').each(($element) => {
-      if ($element.text() === 'Test 123') {
-        cy.get($element).siblings('header').children('[data-qa="delete-button"]').click()
-      }
-    })
   })
+
+  it('passes', () => {
+    cy.get('[data-qa="comment-input"]').eq(0).type(mensagem);
+    cy.get('[data-qa="publish-button"]').eq(0).click();
+    cy.get('[data-qa="comment-text"]').eq(1).should('have.text', mensagem);
+    cy.get('[data-qa="comment-text"]').eq(1).contains(mensagem);
+    cy.get('[data-qa="comment-text"]').each(($element) => {
+      if ($element.text() === mensagem) {
+        cy.get($element).siblings('header').children('[data-qa="delete-button"]').click();
+
+      }
+    });
+  })
+
 })
+
+
+
